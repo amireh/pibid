f22ad12a1b0e3dcc45933b0c806904c4e89e9b99
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-01-22 15:00:12 +0300

    ported user notices

--- app/controllers/user_notices.rb
+++ app/controllers/user_notices.rb
@@ -1,6 +1,6 @@
 route_namespace '/users/:user_id/notices' do
 
-  condition do
+  before do
     restrict_to(:user, with: { id: params[:user_id].to_i })
   end
 
@@ -17,7 +17,7 @@ route_namespace '/users/:user_id/notices' do
     case type
     when "email"
       if @user.email_verified?
-        return erb :"/emails/already_verified"
+        halt 400, 'Already verified.'
       end
 
       if redispatch
@@ -26,12 +26,12 @@ route_namespace '/users/:user_id/notices' do
 
         # notice already sent and is pending?
         if @user.awaiting_email_verification?
-          return erb :"/emails/already_dispatched"
+          halt 400, 'Already dispatched.'
         end
       end
 
       unless @n = @user.verify_email
-        halt 500, "Unable to generate a verification link: #{@user.all_errors}"
+        halt 500, "Unable to generate a verification link: #{@user.report_errors}"
       end
 
       dispatch_email_verification(@user) { |success, msg|
@@ -44,7 +44,7 @@ route_namespace '/users/:user_id/notices' do
     when "password"
       if redispatch
         unless @n = @user.generate_temporary_password
-          halt 500, "Unable to generate temporary password: #{@user.all_errors}"
+          halt 500, "Unable to generate temporary password: #{@user.report_errors}"
         end
 
         dispatch_temp_password(@user) { |success, msg|
@@ -54,38 +54,36 @@ route_namespace '/users/:user_id/notices' do
           end
         }
 
-        flash[:notice] = "Another temporary password message has been sent to your email."
+        200
       end
 
     else # an unknown type
       halt 400, "Unrecognized verification parameter '#{type}'."
     end
 
-    erb :"/emails/dispatched"
+    200
   end
 
   # marks the notice identified by @token as :accepted
   put '/:token' do |token|
     unless @n = @user.notices.first({ salt: token })
-      halt 400, "No such verification link."
+      halt 404, "No such verification link."
     end
 
     case @n.status
     when :expired
-      return erb :"emails/expired"
+      halt 400, 'Expired.'
     when :accepted
-      flash[:warning] = "This verification notice seems to have been accepted earlier."
-      return redirect "/settings/notifications"
+      halt 400, 'Already accepted.'
     else
       @n.accept!
-      case @n.type
-      when 'email'
-        flash[:notice] = "Your email address '#{@n.user.email}' has been verified."
-        return redirect "/settings/account"
-      when 'password'
-        return redirect "/settings/account"
-      end
+      # case @n.type
+      # when 'email'
+      # when 'password'
+      # end
     end
+
+    200
   end
 
 end # namespace['/notices']
