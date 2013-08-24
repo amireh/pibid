5968811bc39a3f573bbade7cddf539d0325cc3cc
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-08-24 12:21:39 +0300

    user reports now requires a disposition of either mail or push

--- app/controllers/user_reports.rb
+++ app/controllers/user_reports.rb
@@ -8,13 +8,19 @@ post '/users/:user_id/reports',
       unless v =~ /\d{4}(?:\/\d{1,2})?(?:\/\d{1,2})?/
         return "Segment must be a string date of the format: YYYY/MM/DD"
       end
+    },
+    disposition: lambda { |v|
+      unless %w[ mail push ].include?(v)
+        return "Disposition must be either 'mail' or 'push'"
+      end
     }
   })
 
   comlink.queue 'reports', 'generate', append_user_to_message({
     client_id: @user.id,
     token: session.id,
-    segment: api_param(:segment)
+    segment: api_param(:segment),
+    disposition: api_param(:disposition)
   })
 
   respond_to do |f|
