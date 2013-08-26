db5d3f4461271037ca62ca85030a941139aa4c46
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-08-26 12:04:43 +0300

    accepting :capture requests on /users/:user_id/captures

--- app/controllers/user_reports.rb
+++ app/controllers/user_reports.rb
@@ -1,3 +1,36 @@
+post '/users/:user_id/captures',
+  provides: [ :json ],
+  requires: [ :user ],
+  auth: [ :user ] do
+
+  api_required!({
+    endpoint: nil,
+    title: nil,
+    disposition: lambda { |v|
+      unless %w[ mail push ].include?(v)
+        return "Disposition must be either 'mail' or 'push'"
+      end
+    }
+  })
+
+  api_optional!({
+    format: lambda { |v|
+      unless %w[ png pdf ].include?(v)
+        return "Format must be one of ['pdf', 'png']"
+      end
+    }
+  })
+
+  comlink.queue 'reports', 'capture', append_user_to_message(api_params({
+    client_id: @user.id,
+    token: session.id
+  }))
+
+  respond_to do |f|
+    f.json { '{}' }
+  end
+end
+
 post '/users/:user_id/reports',
   provides: [ :json ],
   requires: [ :user ],
