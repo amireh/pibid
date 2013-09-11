7552103306a53fe1c7b75c2c4d4c650bdb76306f
Ahmad Amireh <ahmad.amireh@gmail.com>
2013-09-11 12:16:30 +0300

    OER configuration file

--- config/boot.rb
+++ config/boot.rb
@@ -13,7 +13,7 @@ Time.zone = 'UTC'
 
 # ----
 # Validating that configuration files exist and are readable...
-config_files = [ 'application', 'database', 'cookies', 'cors', 'amqp', 'oauth' ]
+config_files = [ 'application', 'database', 'cookies', 'cors', 'amqp', 'oauth', 'oer' ]
 config_files.each { |config_file|
   unless File.exists?(File.join($ROOT, 'config', "%s.yml" %[config_file] ))
     class ConfigFileError < StandardError; end;
